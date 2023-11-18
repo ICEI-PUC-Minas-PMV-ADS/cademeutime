1783df6eab33341f81aeedab2c84c21ec8a3f0ee
Ayrles Sales <ayrlesalespuc@gmail.com>
2023-11-18 03:08:14 -0300

    HTML/CSS Tela descrição de evento - mobile e desktop

--- src/frontend/src/pages/EventDescription/index.jsx
+++ src/frontend/src/pages/EventDescription/index.jsx
@@ -7,6 +7,7 @@ import message from "../../assets/message-circle.svg";
 import home from "../../assets/HomeIconButton.svg";
 import coracao from "../../assets/CoracaoIconButton.svg";
 import pesquisa from "../../assets/PesquisarIconButton.svg";
+import logo from "../../assets/logo.svg";
 import "./stylesPage.css";
 
 
@@ -14,6 +15,10 @@ export function EventDescription() {
 
   return (
     <div className="containerDescription">
+      <div className="logo">
+        <img src={logo} alt="" />
+        <h1>Descrição do Evento</h1>
+      </div>
       <div className="geralDesktop">
         <div className="escopo">
 
@@ -22,31 +27,44 @@ export function EventDescription() {
             <span id="titulo"> Esporte </span>
           </header>
 
-          <form>
-            <div className="inputContainer">
+          <div className="topicos">
+
+            <h1>Esporte</h1>
+            <div className="linha" id="optionsDesktopLinha"></div>
+
+            <div className="Container">
               <text class="options"><img src={mapPin} alt="" />Endereço</text>
+              <text class="options" id="optionsDesktop">Local:</text>
             </div>
 
             <div className="linha"></div>
 
-            <div className="inputContainer">
+            <div className="Container" id="optionsDesktop">
+              <text class="options" id="optionsDesktop">Dia:</text>
+            </div>
+
+            <div className="linha" id="optionsDesktopLinha"></div>
+
+            <div className="Container">
               <text class="options"><img src={schedule} alt="" />00:00</text>
+              <text class="options" id="optionsDesktop">Horário:</text>
             </div>
 
             <div className="linha"></div>
 
-            <div className="inputContainer">
+            <div className="Container">
               <div className="participante">
                 <img src={user} alt="" />
                 <select class="optionsP">
                   <option value="select" disabled selected>Participantes</option>
                 </select>
-                </div>
+              </div>
+              <text class="options" id="optionsDesktop">Participantes:</text>
             </div>
-            
+
             <div className="linha"></div>
 
-            <div className="inputContainer" id="inputContainerComentario">
+            <div className="Container" id="ContainerComentario">
               <label htmlFor="date"><img src={message} alt="" />Comentários</label>
               <textarea
                 type="text"
@@ -55,13 +73,14 @@ export function EventDescription() {
               />
             </div>
 
-            <button className="button">Participar</button>
-            
+            <div className="botao">
+              <button className="button">Participar</button>
+            </div>
 
-              <div className="recusa">
-                <Link to="">Não vou participar</Link>
-              </div>
-          </form>
+            <div className="recusa">
+              <Link to="">Não vou participar</Link>
+            </div>
+          </div>
           <div className="footer">
             <div><button><img src={home} alt="" /></button></div>
             <div><button><img src={coracao} alt="" /></button></div>
