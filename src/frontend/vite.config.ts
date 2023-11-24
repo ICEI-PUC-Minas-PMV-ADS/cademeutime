d3400f08fc0d4662910f6ffc8d001e794b3f1d5c
FernandaAMacieira <famacieira@sga.pucminas.br>
2023-11-24 12:26:46 -0300

    feat: Configurações de deploy

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/pmv-ads-2023-2-e4-aplicdistrib-t5-grupo4-cademeutime",
 })
