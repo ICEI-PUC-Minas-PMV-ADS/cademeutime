555db5bd1ff4218f5b2686a1adc418b8a4860331
katrynribeiro <kakarilber@hotmail.com>
2023-12-02 10:17:55 -0300

    atualizando configurações mobile

--- src/mobile/app/src/main/java/com/example/cademeutime/MainActivity.kt
+++ src/mobile/app/src/main/java/com/example/cademeutime/MainActivity.kt
@@ -4,15 +4,6 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cademeutime.ui.theme.CadeMeuTimeTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -23,7 +14,7 @@ class MainActivity : ComponentActivity() {
 
         webView = findViewById<WebView>(R.id.web) as WebView
 
-        webView.loadUrl("https://ICEI-PUC-Minas-PMV-ADS.github.io/cademeutime")
+        webView.loadUrl("https://icei-puc-minas-pmv-ads.github.io/cademeutime/")
 
         webView.settings.javaScriptEnabled = true
 
