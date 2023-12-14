3445c10068d46ad91592ef48d0fd42df580d6914
Ríder Cantuária <dev.cantuaria@gmail.com>
2023-12-14 13:13:26 -0300

    mudanca de porta

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,7 +1,7 @@
 import ligarServidor from "./servidor.js";
 
 export const server = await ligarServidor();
-export const port = 3000;
+export const port = 4000;
 export const host = '0.0.0.0';
 
 async function main(): Promise<void> {
